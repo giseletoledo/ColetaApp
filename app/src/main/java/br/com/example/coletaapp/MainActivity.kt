8a001f5e7b34ecf6f32f131f38b7c48919a17239
Gisele <gisesonia@gmail.com>
2024-09-23 15:35:13 -0300

    refactor: ajustes de icones e textos em cards

--- app/src/main/java/br/com/example/coletaapp/MainActivity.kt
+++ app/src/main/java/br/com/example/coletaapp/MainActivity.kt
@@ -1,18 +1,52 @@
 package br.com.example.coletaapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import br.com.example.coletaapp.navigation.AppNavigation
+import br.com.example.coletaapp.notifications.LocalNotificationManager
 import br.com.example.coletaapp.ui.theme.ColetaAppTheme
 
+
 class MainActivity : ComponentActivity() {
+
+    // Novo método para gerenciar a solicitação de permissões
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permissão concedida, inicialize as notificações
+            LocalNotificationManager.createNotificationChannel(this)
+        } else {
+            // Permissão negada, mostre uma mensagem ou lide com a recusa
+            Toast.makeText(this, "Permissão para notificações negada.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ColetaAppTheme {
+                // Verifica se a permissão já foi concedida
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Se já concedida, inicialize o canal de notificações
+                    LocalNotificationManager.createNotificationChannel(this)
+                } else {
+                    // Caso contrário, solicite a permissão usando a nova API
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+
                 AppNavigation()  // Inicia a navegação
             }
         }
     }
 }
+
--- app/src/main/java/br/com/example/coletaapp/ui/MainScreen.kt
+++ app/src/main/java/br/com/example/coletaapp/ui/MainScreen.kt
@@ -1,33 +1,84 @@
 package br.com.example.coletaapp.ui
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.com.example.coletaapp.ui.components.BottomNavigationBar
 import br.com.example.coletaapp.ui.components.RoundedButton
 
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    //var notificationCount by remember { mutableStateOf(0) } // Change type to Int
+    val sharedPreferences = context.getSharedPreferences("notification_prefs", Context.MODE_PRIVATE)
+
+    // Estado para o número de notificações ativas
+    val notificationCount = remember { mutableStateOf(getNotificationCountPreference(sharedPreferences)) }
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(text = "Coleta App") },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary, // Cor de fundo da TopAppBar
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary // Cor do título
-                )
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                actions = {
+                    Box(modifier = Modifier.padding(8.dp)) {
+                        IconButton(onClick = {
+                            // Ação de clique no ícone de notificações (opcional)
+                        }) {
+                            BadgedBox(
+                                badge = {
+                                    if (notificationCount.value > 0) {
+                                        Badge {
+                                            Text(text = notificationCount.value.toString())
+                                        }
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Notifications,
+                                    contentDescription = "Notificações",
+                                    tint = Color.Yellow
+                                )
+                            }
+                        }
+                    }
+                },
             )
         },
         bottomBar = {
-            BottomNavigationBar(navController = navController)  // Passando o navController
+            BottomNavigationBar(navController = navController)
         }
     ) { paddingValues ->
         Column(
@@ -36,15 +87,16 @@ fun MainScreen(navController: NavController) {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            // Seção de botões
+            // Primeira linha de botões
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 RoundedButton(
                     icon = Icons.Default.LocationOn,
                     text = "Ecopontos",
-                    onClick = { navController.navigate("addressList") } // navegação
+                    onClick = { navController.navigate("addressList") }
                 )
                 RoundedButton(
                     icon = Icons.Default.Refresh,
@@ -60,7 +112,126 @@ fun MainScreen(navController: NavController) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Outras seções de botões...
+            // Segunda linha de botões
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                RoundedButton(
+                    icon = Icons.Default.Warning,
+                    text = "Manuseio",
+                    onClick = { /* Ação para Manuseio */ }
+                )
+                RoundedButton(
+                    icon = Icons.Default.Info,
+                    text = "Dicas",
+                    onClick = { /* Ação para Dicas */ }
+                )
+                RoundedButton(
+                    icon = Icons.Default.AccountBox,
+                    text = "Campanha",
+                    onClick = { /* Ação para Campanha */ }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Seção "Você sabia?"
+            Text(
+                text = "Você sabia?",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+
+            // Card 1
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically)
+                {
+                    Icon(
+                        imageVector = Icons.Default.Create,
+                        contentDescription = "lápis",
+                        modifier = Modifier
+                            .size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "Desde 2020, com a implementação do Decreto n° 10.388, os consumidores podem descartar medicamentos vencidos ou em desuso em farmácias que possuem pontos de coleta.",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+
+            // Card 2
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "simbolo de informações",
+                        modifier = Modifier
+                            .size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "A reciclagem do óleo de cozinha usado permite a produção de sabão, biodiesel, tintas e outros produtos, contribuindo também para a redução da poluição ambiental.",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+fun NotificationBadge(notificationCount: Int) {
+    if (notificationCount > 0) {
+        IconButton(onClick = { /* Handle notification click */ }) {
+            Icon(Icons.Filled.Notifications, contentDescription = "Notifications")
+            if (notificationCount > 9) {
+                Text(
+                    text = "+9", // Display "+" for high notification counts
+                    color = Color.White,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .size(16.dp)
+                        .background(Color.Red, shape = CircleShape)
+                        .padding(2.dp)
+                )
+            } else {
+                Text(
+                    text = notificationCount.toString(),
+                    color = Color.White,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .size(16.dp)
+                        .background(Color.Red, shape = CircleShape)
+                        .padding(4.dp)
+                )
+            }
+        }
+    } else {
+        Icon(Icons.Filled.Notifications, contentDescription = "Notifications")
+    }
+}
+// Função para obter a contagem de notificações
+private fun getNotificationCountPreference(sharedPreferences: SharedPreferences): Int {
+    return sharedPreferences.getInt("notification_count", 0)
 }
